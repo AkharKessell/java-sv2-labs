980d109c2f4ca3ada78788c720148354fdb11748
AkharKessell <rogkacs@gmail.com>
2021-10-17 14:26:25 +0200

    Name+E-Mail=Data

---
+++ training-solutions/src/main/java/classstructureio/Registration.java
@@ -0,0 +1,19 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg a nevét!");
+        String name = scanner.nextLine();
+        System.out.println("Helló " + name + "!");
+
+        System.out.println("Adja meg az e-mail címét!");
+        String email = scanner.nextLine();
+
+        System.out.println("Az Ön adatai: " + name +" / "+ email);
+    }
+}
