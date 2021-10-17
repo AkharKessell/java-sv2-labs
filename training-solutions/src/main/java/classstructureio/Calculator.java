0acccbf693c3ed28d50d3a66b5ac09292f99f443
AkharKessell <rogkacs@gmail.com>
2021-10-17 14:11:49 +0200

    1+1 Integer output &= Sum

---
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,20 @@
+package classstructureio; //refined
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérlek, add meg az első egész számot!");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Kérlek, add meg a második egész számot!");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("számaid:"+firstNumber+"+"+secondNumber+"=");
+
+        System.out.println(firstNumber+secondNumber);
+    }
+}
