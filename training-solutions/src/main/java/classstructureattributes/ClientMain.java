6a6bb822520145bf2e34ac064daca855e255747c
AkharKessell <rogkacs@gmail.com>
2021-10-17 16:49:43 +0200

    Gyakorlati feladat - Ügyfél nyilvántartás - 1st attempt

---
+++ training-solutions/src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,21 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Scanner systemInScanner = new Scanner(System.in);
+        Client client = new Client();
+
+        System.out.println("Az Ön Neve?");
+        client.name = systemInScanner.nextLine();
+
+        System.out.println("Az Ön Címe?");
+        client.address = systemInScanner.nextLine();
+
+        System.out.println("Az Ön születési éve?");
+        client.YOB = systemInScanner.nextInt();
+
+        System.out.println("#Név:"+client.name + " #Született:"+client.YOB + " #Cím:" + client.address);
+    }
+}
