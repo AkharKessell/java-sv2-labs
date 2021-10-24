27b29c008d9aeb10951db9399f2c5e17d336700e
AkharKessell <rogkacs@gmail.com>
2021-10-24 16:49:50 +0200

    A statements.StatementMain osztály main() metódusában definiálj egy int típusú x változót, melynek értéke az 5 és 6 literál összege.
    Definiálj egy int típusú y változót, mely a 11 literálból kivont x változó értékét kapja.
    
    Definiálj egy int típusú z változót, mely értéke 8.
    
    Definiálj egy boolean típusú b változót, mely értéke true, ha az x értéke nagyobb, mint az y változó értéke.
    
    Definiálj egy boolean típusú c változót, mely értéke true, ha a b értéke true, vagy z értéke nagyobb, mint 5.
    
    A z értékéhez adj hozzá egyet egy operandusú operátorral.
    
    Ellenőrzésként írasd is ki minden változó értékét a konzolra!

---
+++ training-solutions/src/main/java/statements/StatementMain.java
@@ -0,0 +1,19 @@
+package statements;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        int x = 5 + 6;
+        int y = x - 11;
+        int z = 8;
+        boolean b = x > y ? true : false;
+        boolean c = ( b == true || z > 5 ) ? true : false;
+        z++;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(z);
+    }
+}
