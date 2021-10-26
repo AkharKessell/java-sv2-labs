f675a569d986c66f02763ba4a92ba6c2b02c30d2
Gönczi-Kovács Attila <rogkacs@gmail.com>
2021-10-26 19:53:33 +0200

    Írj egy main() metódust a DivByThree, osztályba, mely bekér egy egész számot a felhasználótól, majd kiírja, hogy 3-mal osztható-e!

---
+++ training-solutions/src/main/java/statements/DivByThree.java
@@ -0,0 +1,15 @@
+package src.main.java.statements;
+
+import java.util.Scanner;
+
+public class DivByThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adj meg egy egész számot!");
+        int i = scanner.nextInt();
+
+        System.out.println(i % 3 == 0 ? "osztható" : "nem osztható");
+    }
+}
