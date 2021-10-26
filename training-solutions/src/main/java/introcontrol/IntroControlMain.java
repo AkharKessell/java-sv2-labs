1fd1bf81e2d3254aedbc337ce6f05723082b1b04
Gönczi-Kovács Attila <rogkacs@gmail.com>
2021-10-26 21:47:51 +0200

    A introcontrol.IntroControl osztályban írd meg a következő metódusokat, majd az introcontrol.IntroControlMain osztály main() metódusában teszteld is őket!
    A public int subtractTenIfGreaterThanTen(int number) metódusban add vissza a paraméterként átadott értéket, ha az kisebb vagy egyenlő, mint 10, ellenkező esetben csökkentsd 10-zel, és azt add vissza!
    
    A public String describeNumber(int number) metódusban adj vissza "zero" értéket, ha a paraméterként átadott érték 0, és "not zero" értéket, ha nem 0!
    
    A public String greetingToJoe(String name) metódusban adj vissza Hello Joe értéket, ha a paraméterként átadott név "Joe", és üres Stringet, ha nem!
    
    Az értékesítők 10% jutalékot kapnak az eladások alapján, de csak abban az esetben, ha a havi eladás értéke legalább 1 000 000 Ft. A public int calculateBonus(int sale) metódusban számold ki a jutalékot az eladási összeg alapján, és a jutalék összegét add vissza!
    
    A public int calculateConsumption(int prev, int next) metódusban számold ki a paraméterként megadott mérőóraállások közötti különbséget. Ha a villanyóra eléri a 9999 értéket, átfordul, és újraindul 0 értéktől. Tételezzük fel, hogy csak egyszer fordulhat át, és nem érheti el az előző értéket. Tételezzük fel, hogy 9999 értéknél nagyobbat nem kap paraméterül.
    
    A public void printNumbers(int max) metódussal írd ki a pozitív egész számokat (nullával kezdve) egészen a paraméterként megadott számig (az is legyen kiírva).
    
    A public void printNumbersBetween(int min, int max) metódussal írd ki a pozitív egész számokat a két paraméterként megadott érték között (a megadott értékeket is beleértve). Feltételezzük, hogy mindkét paraméterként kapott szám nagyobb vagy egyenlő nullával.
    
    A public void printNumbersBetweenAnyDirection(int a, int b) metódussal írd ki a pozitív egész számokat a két paraméterként megadott érték között (a megadott értékeket is beleértve). Ha az a értéke nagyobb, mint a b értéke, akkor csökkenő sorrendben történjen a kiíratás.
    
    A public void printOddNumbers(int max) metódussal írd ki a páratlan pozitív egész számokat (egytől indítva) egészen a paraméterként megadott számig (az is legyen kiírva, ha páratlan)!

---
+++ training-solutions/src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,49 @@
+package src.main.java.introcontrol;
+
+public class IntroControlMain {
+
+    public static void main(String[] args) {
+        IntroControl introControl = new IntroControl();
+
+        System.out.println("-10 < 10");
+        System.out.println(introControl.subtractTenIfGreaterThanTen(0));
+        System.out.println(introControl.subtractTenIfGreaterThanTen(10));
+        System.out.println(introControl.subtractTenIfGreaterThanTen(15));
+
+        System.out.println("describeNumber");
+        System.out.println(introControl.describeNumber(0));
+        System.out.println(introControl.describeNumber(2));
+
+        System.out.println("greetingToJoe");
+        System.out.println(introControl.greetingToJoe("Joe"));
+        System.out.println(introControl.greetingToJoe("Jane"));
+
+        System.out.println("calculateBonus");
+        System.out.println(introControl.calculateBonus(0));
+        System.out.println(introControl.calculateBonus(100));
+        System.out.println(introControl.calculateBonus(1_000_000));
+        System.out.println(introControl.calculateBonus(1_000_090));
+
+        System.out.println("calculateConsumption");
+        System.out.println(introControl.calculateConsumption(10, 20));
+        System.out.println(introControl.calculateConsumption(9990, 10));
+
+        System.out.println("printNumbers");
+        introControl.printNumbers(5);
+
+        System.out.println("printNumbersBetween");
+        introControl.printNumbersBetween(10, 5);
+
+        System.out.println("printNumbersBetweenAnyDirection");
+        introControl.printNumbersBetweenAnyDirection(10, 15);
+        System.out.println("printNumbersBetweenAnyDirection-backwards");
+        introControl.printNumbersBetweenAnyDirection(15, 10);
+
+        System.out.println("printOddNumbers - 1");
+        introControl.printOddNumbers(3);
+        System.out.println("printOddNumbers - 10");
+        introControl.printOddNumbers(5);
+        System.out.println("printOddNumbers - 11");
+        introControl.printOddNumbers(6);
+    }
+}
