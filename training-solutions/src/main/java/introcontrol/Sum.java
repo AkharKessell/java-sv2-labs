be0d9f4739386bbdbc2643dddb5847515babcc33
Gönczi-Kovács Attila <rogkacs@gmail.com>
2021-10-27 11:17:07 +0200

    Kérj be a felhasználótól ciklusban öt számot, és számold ki az összegüket. A Sum osztályba dolgozz!

---
+++ training-solutions/src/main/java/introcontrol/Sum.java
@@ -0,0 +1,17 @@
+package src.main.java.introcontrol;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Add meg az " + (i + 1) + ". számot:");
+            int number = scanner.nextInt();
+            sum += number;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
