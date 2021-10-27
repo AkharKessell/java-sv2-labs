22d3b146f7bcd3a5772f30fe086b91d220bc97f3
Gönczi-Kovács Attila <rogkacs@gmail.com>
2021-10-27 14:49:20 +0200

    A cmdarguments.verse.VerseMain osztály main() metódusának parancssori paraméterként add át egy kedvenc verssorodat szavanként! A main() metódusban először írd ki a konzolra a verssort! Majd írd ki a szavakat fordított sorrenben, szóközökkel elválasztva!

--- training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
+++ training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
@@ -1,4 +1,15 @@
 package src.main.java.cmdarguments.verse;
 
 public class VerseMain {
+    public static void main(String[] args) {
+        for (String i: args) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (var i = args.length - 1; i >= 0; i--) {
+            System.out.print(args[i] + " ");
+        }
+    }
+
 }
