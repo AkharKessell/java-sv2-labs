f082efefd8ab98a5709a836154b91b7c2370fa40
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-02 11:31:50 +0100

    Hozz létre egy Favourites nevű osztályt, amelynek main() metódusában hozz létre egy List<String> favouriteThings listát! Ezután kérd be a felhasználótól néhány kedvencét, például a kedvenc filmjét, színészét, állatát, stb., amelyeket aztán tárolj is el a favouriteThings listában! Végül írd ki a konzolra a lista méretét és tartalmát!

---
+++ training-solutions/src/main/java/aslist/Favourites.java
@@ -0,0 +1,36 @@
+package main.java.aslist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Favourites {
+
+        public static void main(String[] args) {
+            Favourites favourites = new Favourites();
+            Scanner scanner = new Scanner(System.in);
+            List<String> favouriteThings = new ArrayList<>();
+
+            System.out.println("We need a list for your favourite things. PUSH!");
+            scanner.nextLine();
+
+            System.out.println("Favourite movie?");
+            String movie = scanner.nextLine();
+            favouriteThings.add(movie);
+
+            System.out.println("Favourite color?");
+            String actor = scanner.nextLine();
+            favouriteThings.add(actor);
+
+            System.out.println("Favourite car?");
+            String colour = scanner.nextLine();
+            favouriteThings.add(colour);
+
+            System.out.println("Favourite animal?");
+            String animal = scanner.nextLine();
+            favouriteThings.add(animal);
+
+            System.out.println(favouriteThings.size());
+            System.out.println(favouriteThings);
+        }
+    }
