f1a956aff3feb82d9e022c14ffe0a17638bc6346
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-02 11:31:30 +0100

    Ha csak 3 dolgot vihetnél magaddal egy lakatlan szigetre, melyik lenne az a három? Jól gondold meg a választ, majd az Island nevű osztály main() metódusában létrehozott List<String> importantThings listában tárold is el őket! A lista ne legyen bővíthető, hiszen csak három dolgot vihetsz magaddal. Ezután először írd ki ezeket a dolgokat a konzolra, majd implementáld azt, hogy mégis meggondoltad magad és egy dolgot lecserélsz a három közül! Végül írd is ki a konzolra a végleges döntésed eredményét!

---
+++ training-solutions/src/main/java/aslist/Island.java
@@ -0,0 +1,26 @@
+package main.java.aslist;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Island {
+    public static void main(String[] args) {
+        Island island = new Island();
+        Scanner scanner = new Scanner(System.in);
+        List<String> importantThings = Arrays.asList("water", "shovel", "radio");
+
+        System.out.println("You can only bring 3 things?");
+        System.out.println("You have: " + importantThings);
+        System.out.println();
+        System.out.println("Choose another thing instead, choose a number!");
+        System.out.println("Which one:");
+        int index = scanner.nextInt() - 1;
+        scanner.nextLine();
+        System.out.println("For what?");
+        String thing = scanner.nextLine();
+        importantThings.set(index, thing);
+        System.out.println("You bring:");
+        System.out.println(importantThings);
+    }
+}
