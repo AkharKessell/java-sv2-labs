5601fa379959893fcafbf34f589ade4ecfac9b9f
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-03 13:00:46 +0100

    Készíts egy Sequence nevű osztályt, amelynek main() metódusában példányosíts le egy List<Integer> elements listát oly módon, hogy már eleve legyen benne két tetszőleges szám, de legyen lehetőség arra, hogy továbbiakat is hozzáadhass! Ciklusban adj néhány új elemet a listához úgy, hogy minden következő elem a sorban előtte lévő két szám szorzata legyen!
    Ha például a példányosított lista kezdeti elemei a következők: [1, 2], akkor öt új elem hozzáadása után a listának a következőket kell tartalmaznia: [1, 2, 2, 4, 8, 32, 256].
    
    Végül írd ki a konzolra a lista tartalmát és a méretét is!

---
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -0,0 +1,16 @@
+package src.main.java.aslist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sequence {
+    public static void main(String[] args) {
+        List<Integer> elements = new ArrayList(Arrays.asList(2, 4));
+            for (int i = 0; i < 5; i++) {
+                elements.add(elements.get(i) * elements.get(i+1));
+        }
+        System.out.println(elements);
+        System.out.println("Size : " + elements.size());
+    }
+}
