dbecd4c96e16bca52742f13c9664629848e519a1
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-03 13:41:31 +0100

    Hozz létre egy StudyGroup osztályt, melynek printStudyGroups(List<String> students) metódusa a kapott listában szereplő tanulókat két külön tanulócsoportra osztja, és ki is írja a konzolra, hogy ki melyik csoportba kerül. Az 1-es csoportba kerüljenek azok, akiknek a neve legfeljebb 10 betű hosszú, a 2-es csoportba pedig az ennél hosszabb nevűek! Az osztály main() metódusában példányosítsd le a listát, add meg benne a tanulók neveit, majd oszd őket két csoportba (hívd meg a listán a metódust)!

---
+++ training-solutions/src/main/java/looptypes/StudyGroup.java
@@ -0,0 +1,17 @@
+package src.main.java.looptypes;
+
+import java.util.List;
+
+public class StudyGroup {
+    public void printStudyGroups(List<String> students) {
+
+        for (String name: students) {
+
+            if (name.length() <= 10) {
+                System.out.println("Group 01: " + name);
+            } else {
+                System.out.println("Group 02: " + name);
+            }
+        }
+    }
+}
---
+++ training-solutions/src/main/java/looptypes/StudyGroupMain.java
@@ -0,0 +1,13 @@
+package src.main.java.looptypes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StudyGroupMain {
+    public static void main(String[] args) {
+        StudyGroup group = new StudyGroup();
+
+        List<String> students = Arrays.asList("Viczián István", "Bárczay Krisóf", "Kovács Enikő","Tóth Enikő");
+        group.printStudyGroups(students);
+    }
+}
