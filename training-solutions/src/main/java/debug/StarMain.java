02b1b33425de5ae91554bd8dda9e966509e812c1
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-03 18:00:49 +0100

    Hozd létre a Star osztályt, amelybe másold bele a következő két metódust!Majd az osztály main() metódusában példányosíts egy Star példányt, és definiálj egy olyan egész számokból álló tömböt, amelyben csak nemnegatív számok vannak, valamint a számok növekvő sorrendben követik egymást! Ha ezután meghívod a writeStars() metódust, aminek paraméterül átadod ezt a tömböt, akkor a metódus jelenlegi formájában sorban kiírja a tömb számainak megfelelő számú csillagot, mindegyiket külön sorba. Tehát pl. a következő számokat tartalmazó tömb esetén: 1, 3, 5, 10, 12                                                                          a következő lesz a konzolon az eredmény:
                                                                             *
                                                                             ***
                                                                             *****
                                                                             **********
                                                                             ************
    
                                                                             A debugger segítségével keresd meg, mit kellene változtatni a metóduson ahhoz, hogy ugyanezen számokat tartalmazó tömb esetén a következő eredményt kapd (minden számból kivonja az előtte lévőt, és a kapott eredménynek megfelelő számú csillagot írja ki a konzolra):
    
                                                                             *
                                                                             **
                                                                             **
                                                                             *****
                                                                             **

---
+++ training-solutions/src/main/java/debug/StarMain.java
@@ -0,0 +1,10 @@
+package src.main.java.debug;
+
+public class StarMain {
+    public static void main(String[] args) {
+        Star star = new Star();
+
+        int [] numbers = {1, 3, 5, 10, 12};
+        star.writeStars(numbers);
+    }
+}
