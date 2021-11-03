139b3bae00786f5d053e63f2f88772800540ceb0
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-03 18:15:54 +0100

    Írj egy finalmodifier.TaxCalculator osztályt, mely tartalmazza az ÁFA értékét, ami 27%. Írj egy double tax(double price) metódust, mely a paraméterként megadott érték ÁFA értékét számolja, és egy double priceWithTax(double) metódust, mely az árat adja vissza az ÁFA-val együtt!

---
+++ training-solutions/src/main/java/finalmodifier/TaxCalculator.java
@@ -0,0 +1,14 @@
+package src.main.java.finalmodifier;
+
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static final int TAX =27;
+
+    public double tax(double price){
+        return price * (TAX / 100.0);
+    }
+    public double priceWithTax(double price) {
+        return price + tax(price);
+    }
+}
---
+++ training-solutions/src/main/java/finalmodifier/TaxMain.java
@@ -0,0 +1,15 @@
+package src.main.java.finalmodifier;
+
+import java.util.Scanner;
+
+public class TaxMain {
+    public static void main(String[] args) {
+        TaxCalculator calc = new TaxCalculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Net price:");
+        double price = scanner.nextDouble();
+        System.out.println("Net: " + price);
+        System.out.println("Tax: " + calc.tax(price));
+        System.out.println("Gross :" + calc.priceWithTax(price));
+    }
+}
