85f24f130b1a0eddb3c4731f7c7365c8161b2106
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-03 18:35:23 +0100

    Definiálj egy finalmodifier.Gentleman osztályt, melyben definiáld kvázi konstansként a MESSAGE_PREFIX változót, mely a köszönés elejét tartalmazza! Írj egy String sayHello(String name) metódust, mely a MESSAGE_PREFIX értékét összefűzi a name paraméter értékével és az eredményt visszaadja!

---
+++ training-solutions/src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,13 @@
+package src.main.java.finalmodifier;
+
+public class Gentleman {
+    public static final String MESSAGE_PREFIX = "Hello";
+    public static String sayHello(String name) {
+        return MESSAGE_PREFIX + name;
+    }
+
+    public static void main(String[] args) {
+        String name = " John";
+        System.out.println(sayHello(name));
+    }
+}
