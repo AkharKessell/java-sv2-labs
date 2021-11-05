b1eef2e3915618cf71c096110a8823a090ca92cc
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-05 12:02:24 +0100

    Gyakorlati feladat - Regisztráció
    A feladat egy egyszerű regisztrációs alkalmazás készítése az intromethods.registration csomagba.
    
    Ehhez először készíts egy Person osztályt, melynek három attribútuma a következő: String name, LocalDate dateOfBirth és String email. Mindegyikhez legyen getter, valamint legyen az osztálynak egy konstruktora, mely mindhárom attribútum értékét várja. Legyen ezenkívül egy toString() metódusa, mely ne az IDE által generált szöveget adja vissza, hanem legyen alkalmas arra, hogy a személy regisztrációs adatait könnyen olvasható formában megjelenítse!
    
    Készíts ezután egy Registration osztályt, melynek main() metódusába írd bele a regisztrációs folyamatot! A felhasználótól kérd be sorban az adatait a következő módon:
    
        Kérd be külön a vezeték- és külön a keresztnevét, majd írj egy metódust a Registration osztályban, amely ezt a két adatot várja paraméterül, és visszaadja a személy teljes nevét!
        Kérd be a születési adatait úgy, hogy külön az évet, külön a hónapot és külön a napot, majd írj egy metódust (szintén a Registration osztályba), mely ezeket várja paraméterül, és visszaadja a személy születési dátumát, LocalDate típusként.
        Majd kérd be az e-mail címét.
    
    Végül példányosíts le egy Person-t, amelyhez a Registration osztályban megírt metódusok által visszaadott értékeket használd, és írd ki a felhasználónak (a Person-ben felülírt toString() segítségével), hogy milyen adatokkal regisztrált!

---
+++ training-solutions/src/main/java/intromethods/registration/RegistrationMain.java
@@ -0,0 +1,40 @@
+package src.main.java.intromethods.registration;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class RegistrationMain {
+    public String getFullName(String lastName, String firstName) {
+        return lastName + " " + firstName;
+    }
+
+    public LocalDate getDateOfBirth(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First Name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Last Name:");
+        String lastName = scanner.nextLine();
+        String fullName = new RegistrationMain().getFullName(firstName, lastName);
+
+        System.out.println("YOB:");
+        int year = scanner.nextInt();
+        System.out.println("Month:");
+        int month = scanner.nextInt();
+        System.out.println("Day:");
+        int day = scanner.nextInt();
+        LocalDate dateOfBirth = new RegistrationMain().getDateOfBirth(year, month, day);
+
+        System.out.println("Email:");
+        scanner.nextLine();
+        String email = scanner.nextLine();
+
+        Person person = new Person(fullName, dateOfBirth, email);
+
+        System.out.println("Data: " + person.toString());
+    }
+}
