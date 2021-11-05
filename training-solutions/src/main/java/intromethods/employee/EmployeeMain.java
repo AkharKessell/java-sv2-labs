0d629dce6ac8e2eeb47e822cb6cc04b75b3fcb6f
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-05 10:00:16 +0100

    Gyakorlati feladat - Alkalmazottak
    Készítsd el az intromethods.employee csomagba az Employee osztályt, amelyben az alkalmazott nevét (name), belépési évét (hiringYear) és az egész értékű fizetését (salary) tárolod attribútumként!
    
    Az osztály példányosításakor mindhárom adatot meg kell adni, a konstruktort ennek megfelelően készítsd el!
    
    Minden attribútumához készíts gettert, valamint a name attribútumhoz settert is! A fizetése utólag emelhető, ezért készíts egy raiseSalary() metódust, amely paraméterként megkapja az emelés mértékét forintban, és ennek megfelelően módosítja a fizetést!
    
    Generáld le az IDE segítségével a toString() metódust! Milyen alakban jelenik meg egy objektum állapota? Teszteld az osztály metódusait az EmployeeMain osztály main() metódusában!

---
+++ training-solutions/src/main/java/intromethods/employee/EmployeeMain.java
@@ -0,0 +1,16 @@
+package src.main.java.intromethods.employee;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("John Doe", 2009, 100_000);
+        System.out.println(employee.getName() + " " + employee.getHiringYear()+ " " +employee.getSalary());
+        employee.setName("Jack Doe");
+        System.out.println(employee.getName() + " " + employee.getHiringYear()+ " " +employee.getSalary());
+
+        employee.raiseSalary(50_000);
+
+        System.out.println(employee.getName() + " " + employee.getHiringYear()+ " " +employee.getSalary());
+        System.out.println(employee.toString());
+    }
+}
