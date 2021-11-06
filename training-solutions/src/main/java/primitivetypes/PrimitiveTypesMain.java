23969c861910667b21daf40b15f5ffef4798b885
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-06 15:04:56 +0100

    A primitivetypes.PrimitiveTypesMain osztály main() metódusába dolgozz!
    Egész szám típusú változók létrehozása lesz a feladat, próbáld meg követni közben, hogy hol történik a típusnak megfelelő értékadás, hol történik autoboxing és hol autounboxing!
    
        a változó: a típusa legyen int, az értéke 2!
        b változó: a típusa legyen Integer, az értéke 2!
        c változó: a típusa legyen Integer, az értéke legyen az a változó értéke!
        d változó: a típusa legyen Integer, az értéke legyen a b változó értéke!
        e változó: a típusa legyen Integer, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új Integer objektumot a 2-es számból!
        f változó: a típusa legyen int, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új Integer objektumot a 2-es számból!
        g változó: a típusa legyen int, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új Integer objektumot az a változó értékéből!
        h változó: a típusa legyen int, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új Integer objektumot a c változó értékéből!
        i változó: a típusa legyen int, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új int típusú értéket a "2" szövegből!
        j változó: a típusa legyen Integer, az értékét úgy add meg, hogy az Integer osztály megfelelő metódusával hozz létre egy új int típusú értéket a "2" szövegből!
    
    Minden létrehozott változó értékét írasd is ki a konzolra!
    Gyakorlati feladat - Logikai értékek
    
    A primitivetypes.PrimitiveTypesMain osztály main() metódusába dolgozz!
    
    Logikai típusú változók létrehozása lesz a feladat, próbáld meg követni közben, hogy hol történik a típusnak megfelelő értékadás, hol történik autoboxing és hol autounboxing!
    
        k változó: a típusa legyen boolean, az értéke true!
        l változó: a típusa legyen Boolean, az értéke true!
        m változó: a típusa legyen boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új Boolean objektumot a true logikai literálból!
        n változó: a típusa legyen Boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új Boolean objektumot a true logikai literálból!
        o változó: a típusa legyen boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új boolean típusú értéket a "TrUe" szövegből!
        p változó: a típusa legyen Boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új boolean típusú értéket a "TrUe" szövegből!
        q változó: a típusa legyen boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új boolean típusú értéket a "TreU" szövegből!
        r változó: a típusa legyen Boolean, az értékét úgy add meg, hogy a Boolean osztály megfelelő metódusával hozz létre egy új boolean típusú értéket a "TreU" szövegből!
    
    Minden létrehozott változó értékét írasd is ki a konzolra!

---
+++ training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,25 @@
+package src.main.java.primitivetypes;
+
+public class PrimitiveTypesMain {
+    public static void main(String[] args) {
+        int a = 2;
+        Integer b = 2;
+        Integer c = a;
+        Integer d = b;
+        Integer e = Integer.valueOf(2);
+        int f = Integer.valueOf(2);
+        int g = Integer.valueOf(a);
+        int h = Integer.valueOf(c);
+        int i = Integer.parseInt("2");
+        Integer j = Integer.parseInt("2");
+        //-------------------------------------
+        boolean k = true;
+        Boolean l = true;
+        boolean m = Boolean.parseBoolean("true");
+        Boolean n = Boolean.parseBoolean("true");
+        boolean o = Boolean.parseBoolean("TrUe");
+        Boolean p = Boolean.parseBoolean(("TrUe"));
+        boolean q = Boolean.parseBoolean("TreU");
+        Boolean r = Boolean.parseBoolean("TreU");
+    }
+}
