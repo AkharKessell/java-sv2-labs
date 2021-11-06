e8397bfc78edaa3432ac34379fc8d860a43424aa
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-06 09:21:10 +0100

    Készíts egy dogs.Dog osztályt, amelynek három attribútuma van: String name, int age, String colour Legyen mindegyik attribútumhoz getter és setter, valamint legyen az osztálynak egy konstruktora, mely mindhárom attribútum értékét várja! Írd felül a toString() metódusát is, szabadon dönthetsz, hogy az IDE által generált értéket adja vissza, vagy egy általad megadott értéket, a fontos az, hogy mindhárom attribútum értéke benne legyen a visszaadott szövegben.
    Készíts ezután egy Kennel osztályt, melynek main() metódusában írd meg a következőket, és közben próbáld meg követni, mi is történik a háttérben, a memóriában:
    
        Hozz létre egy üres List<Dog> dogs listát, amelyben az osztály a hozzá tartozó kutyákat fogja nyilvántartani!
        Példányosíts három különböző Dog-ot, és mindhármat add hozzá a listához!
        Írd ki a konzolra a lista tartalmát!
        Majd definiálj egy új (negyedik) Dog típusú változót, amelynek add értékül a lista 1-es indexű elemét! A most definiált változónév használatával változtasd meg a kutya színét!
        Újra írd ki a konzolra a lista tartalmát!
        Definiálj egy újabb (ötödik) Dog típusú változót, melynek adj értékül egy újonnan példányosított objektumot! Írd is ki a konzolra ennek a példánynak az adatait!
        Az előző lépésben definiált változónak add értékül a kutyalista 1-es indexű elemét! Írd ki a konzolra a változó értékét!
        A listához add hozzá az előző változó által hivatkozott példányt! Írd ki a konzolra a lista tartalmát!
        Definiálj megint egy (hatodik) Dog típusú változót, majd add neki értékül a korábbi, negyedik Dog típusú változó értékét! A most definiált változó nevének használatával változtasd meg a kutya színét!
        Újra írd ki a konzolra a lista tartalmát!
        Definiálj egy második List<Dog> típusú listát, melynek add értékül az első listát!
        Definiálj megint egy (hetedik) Dog típusú változót, majd ennek is add értékül a korábbi, negyedik Dog típusú változó értékét! A most definiált változó nevének használatával újra változtasd meg a kutya színét!
        Újra írd ki a konzolra a lista tartalmát, immár a második List<Dog> típusú változó nevének használatával!
        Az eredetileg létrehozott, másodikként definiált Dog típusú változó nevének használatával változtasd meg a kutya színét! Írd ki újra a konzolra a lista tartalmát, a második List<Dog> típusú változó nevének használatával!
        Az eredetileg létrehozott, másodikként definiált Dog típusú változó értékét változtasd meg null-ra! Írd ki újra a konzolra a lista tartalmát, a második List<Dog> típusú változó nevének használatával!
        Definiálj egy újabb (nyolcadik) Dog típusú változót, majd add neki értékül az első kutyalista 1-es indexű elemét! Majd állítsd át az értékét null-ra! Írd ki újra a konzolra a lista tartalmát, a második List<Dog> típusú változó nevének használatával!
        Definiálj egy utolsó (kilencedik) Dog típusú változót, majd add neki értékül az első lista 1-es indexű elemét! A most definiált változó nevének használatával változtasd meg a kutya színét! Írd ki újra a konzolra a lista tartalmát, a második List<Dog> típusú változó nevének használatával!
    
    Tudtad követni, mi történik a memóriában?

---
+++ training-solutions/src/main/java/references/dogs/Kennel.java
@@ -0,0 +1,54 @@
+package src.main.java.references.dogs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kennel {
+    public static void main(String[] args) {
+        List<Dog> dogs = new ArrayList<>();
+
+        Dog dog01 = new Dog("Spikey", 4, "White");
+        dogs.add(dog01);
+        Dog dog02 = new Dog("Hektor", 5, "Black");
+        dogs.add(dog02);
+        Dog dog03 = new Dog("Satan", 6, "Brown");
+        dogs.add(dog03);
+        System.out.println(dogs);
+
+        Dog dog04 = dogs.get(1);
+        dog04.setColour("Yellow");
+        System.out.println(dogs);
+
+        Dog dog05 = new Dog("Berny", 7, "spotted");
+        System.out.println(dog05);
+
+        dog05 = dogs.get(1);
+        System.out.println(dog05);
+
+        dogs.add(dog05);
+        System.out.println(dogs);
+
+        Dog dog06 = dog04;
+        dog06.setColour("Pink");
+        System.out.println(dogs);
+
+        List<Dog> dogs02 = dogs;
+        Dog dog07 = dog04;
+        dog07.setColour("ChestNut");
+        System.out.println(dogs02);
+
+        dog02.setColour("Stripped");
+        System.out.println(dogs02);
+
+        dog02 = null;
+        System.out.println(dogs02);
+
+        Dog dog08 = dogs.get(1);
+        dog08 = null;
+        System.out.println(dogs02);
+
+        Dog dog09 = dogs.get(1);
+        dog09.setColour("Squared");
+        System.out.println(dogs02);
+    }
+}
