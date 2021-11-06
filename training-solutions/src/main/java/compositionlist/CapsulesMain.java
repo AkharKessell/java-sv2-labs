e260b1350a698280cee3d108ca789e9b8fd75e21
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-06 12:52:29 +0100

    Gyakorlati feladat - Kávékapszulák
    Készíts egy Capsules osztályt, mely segít olyan műalkotás megtervezésében, amely újrahasznosított kávékapszulákból áll. Egy hajlítható műanyag csőbe tudjuk helyezni a különböző színű kapszulákat egymás mellé. Így alakul ki az alkotás, amit aztán különböző formára hajlíthatunk.
    
    A Capsules osztály egy ArrayList típusú attribútumban tárolja a betett kapszulákat, méghozzá a színüket String-ként.
    
    A Capsules osztálynak legyen egy addLast(String), addFirst(String), removeFirst(), removeLast() metódusa, mely betesz, illetve kivesz kapszulákat a csőből. Legyen egy List<String> getColors() metódusa, mely visszaadja a kapszulákat tartalmazó listát, hogy ki lehessen írni.
    
    Írj egy main() metódust, mely teszteli a metódusok működését.

---
+++ training-solutions/src/main/java/compositionlist/CapsulesMain.java
@@ -0,0 +1,21 @@
+package src.main.java.compositionlist;
+
+public class CapsulesMain {
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+
+        capsules.addFirst("black");
+        capsules.addLast("white");
+        capsules.addLast("yellow");
+        capsules.addFirst("grey");
+        capsules.addFirst("red");
+        System.out.println(capsules.getColours());
+
+        capsules.removeLast();
+        capsules.removeFirst();
+        capsules.removeFirst();
+        capsules.removeLast();
+
+        System.out.println(capsules.getColours());
+    }
+}
