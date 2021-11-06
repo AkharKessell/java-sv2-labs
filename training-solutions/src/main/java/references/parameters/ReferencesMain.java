4b961cd1fa80b0d2357984a5e1d7d434e2a1d5e1
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-06 08:45:30 +0100

    Készíts egy Person osztályt a references.parameters csomagba, melyben eltárolod a nevét (name) és az életkorát (age)! A konstruktor mindkét attribútumot megkapja, és mindenhez generálj gettert és settert!
    Deklarálj a ReferencesMain osztály main() metódusában két Person típusú változót! Példányosíts egy új Person objektumot, és add értékül az első változónak! A második változónak add értékül az elsőt! Módosítsd a második változón át az objektum name attribútumát! Írd ki mindkét változó tartalmát a képernyőre! Mit tapasztalsz? Vajon mi történt?
    
    Deklarálj két egész típusú változót! Az elsőnek add értékül a 24-et! A második változónak add értékül az elsőt, majd növeld meg a második változó értékét 1-gyel! Írd ki mindkét változót a képernyőre! Mit tapasztalsz? Miért?
    
    Készíts egy új Person objektumot és add értékül az egyik Person típusú változónak! Írd ki mindkét változó tartalmát a képernyőre! Mit tapasztalsz? Miért?
    
    Próbáld követni, hogy mi történik a memóriában! Segítségedre lesz a debugger.

---
+++ training-solutions/src/main/java/references/parameters/ReferencesMain.java
@@ -0,0 +1,22 @@
+package src.main.java.references.parameters;
+
+public class ReferencesMain {
+    public static void main(String[] args) {
+
+        Person person01;
+        Person person02;
+        person01 = new Person("John",22);
+        person02 = person01;
+        person02.setName("Jack");
+        System.out.println(person01.getName());
+        System.out.println(person02.getName());
+        int i = 24;
+        int a = i;
+        a++;
+        System.out.println(i);
+        System.out.println(a);
+        person01=new Person("Jill", 15);
+        System.out.println(person01.getName());
+        System.out.println(person02.getName());
+    }
+}
