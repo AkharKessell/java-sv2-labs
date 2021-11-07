2cac04ffa895c617c52ac79fe36111fcf18f7479
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 19:11:37 +0100

    Gyakorlati feladat - Jó végeredmény
    Az int típus ábrázolási tartományának határa pozitív irányban 2_147_483_647. Készíts a TooBigNumber osztályban egy getRightResult(int number) metódust, amelyben ehhez a számhoz hozzáadod a paraméterként kapott számot! A feladat az, hogy a metódus matematikailag helyes eredményt adjon vissza!
    
    Teszteld az osztály main() metódusában!

---
+++ training-solutions/src/main/java/conversions/TooBigNumber.java
@@ -0,0 +1,9 @@
+package src.main.java.conversions;
+
+public class TooBigNumber {
+
+    public long getRightResult(int number){
+        return number + 2_147_483_647L;
+    }
+
+}
---
+++ training-solutions/src/main/java/conversions/TooBigNumberMain.java
@@ -0,0 +1,8 @@
+package src.main.java.conversions;
+
+public class TooBigNumberMain {
+    public static void main(String[] args) {
+        TooBigNumber a = new TooBigNumber();
+        System.out.println(a.getRightResult(10));
+    }
+}
