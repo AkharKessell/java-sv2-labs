4e8e36f6c3b148e9682f990d3a966647a41615e2
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 21:44:44 +0100

    Készíts egy filename.FileNameManipulator osztályt, amely fájlnevek ellenőrzésére, illetve ehhez kapcsolódó String műveletekre alkalmas metódusokat tartalmaz.
    Tippek:
    
    Ha igény van rá, alkalmazd a metódusok láncolását! Figyelj a vezető és követő whitespace karakterekre!
    
    Teszteld a megírt metódusokat az osztály main() metódusában!

---
+++ training-solutions/src/main/java/stringmethods/filename/FileNameManipulator.java
@@ -0,0 +1,32 @@
+package src.main.java.stringmethods.filename;
+
+public class FileNameManipulator {
+
+    public char findLastCharacter(String str) {
+        char[] script = str.toCharArray();
+        return script[script.length - 1];
+    }
+
+    public String findFileExtension(String fileExtension) {
+        return fileExtension.substring(fileExtension.indexOf(".") + 1);
+    }
+
+    public boolean identifyFilesByExtension(String ext, String fileName) {
+        return fileName.contains(ext);
+    }
+
+    public boolean compareFilesByName(String searchedFileName, String actualFileName) {
+        return searchedFileName.equalsIgnoreCase(actualFileName);
+    }
+
+    public String changeExtensionToLowerCase(String fileName) {
+        String firstPart = fileName.substring(0, fileName.indexOf("."));
+        int dotIndex = fileName.indexOf(".");
+        String end = fileName.substring(dotIndex).toLowerCase();
+        return firstPart + end;
+    }
+
+    public String replaceStringPart(String fileName, String present, String target) {
+        return fileName.replace(target, present);
+    }
+}
---
+++ training-solutions/src/main/java/stringmethods/filename/FileNameManipulatorMain.java
@@ -0,0 +1,15 @@
+package src.main.java.stringmethods.filename;
+
+public class FileNameManipulatorMain {
+    public static void main(String[] args) {
+        FileNameManipulator fileNameManipulator = new FileNameManipulator();
+
+        System.out.println(fileNameManipulator.findLastCharacter("box"));
+        System.out.println(fileNameManipulator.findFileExtension("readme.txt"));
+        System.out.println(fileNameManipulator.identifyFilesByExtension("exe", "test.exe"));
+        System.out.println(fileNameManipulator.compareFilesByName("music.mp3", "music.MP3"));
+        System.out.println(fileNameManipulator.changeExtensionToLowerCase("run.BAT"));
+        System.out.println(fileNameManipulator.replaceStringPart("Doom.bat", "bat", "exe"));
+    }
+
+}
