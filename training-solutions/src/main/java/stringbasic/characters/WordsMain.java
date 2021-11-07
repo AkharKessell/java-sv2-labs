0087c6a5fea1134268248db207674ffca3b23a4b
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 20:44:21 +0100

    Gyakorlati feladat - Online oktatás
    Ebben a feladatban írj meg egy nagyon egyszerű programot, amely online vagy interaktív oktatás közben használható (általános iskola első osztályosoknál, a magyar, mint idegennyelv tanításánál vagy tíz ujjas vakon gépelés tanulásához).
    
    A characters.Words osztályban legyen az alkalmazás!
    
    A menete az, hogy a tanár megad egy szót, amelyet az alkalmazás kér be tőle. Ezután a program írja ki a konzolra, hogy ezt a szót le kell betűznie a tanulónak. Majd az alkalmazás az egyenként beírt betűket írja ki a konzolra, írja ki a belőlük összeállítható szót, és végül értékelje ki, hogy jól oldotta-e meg a tanuló a feladatot.

---
+++ training-solutions/src/main/java/stringbasic/characters/WordsMain.java
@@ -0,0 +1,32 @@
+package src.main.java.stringbasic.characters;
+
+import java.util.Scanner;
+
+public class WordsMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Word to be spelled: ");
+        String word = scanner.nextLine();
+
+        String wordToSpell = "";
+        System.out.println("Time to spell the word " + word);
+
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println("Type letter number " + (i + 1));
+            String num = scanner.nextLine();
+                wordToSpell += num;
+                System.out.println("You typed the letter " + num);
+        }
+
+        System.out.println("You spelled the word: " + wordToSpell);
+
+        if (word.equals(wordToSpell)) {
+            System.out.println("You are correct");
+
+        } else {
+            System.out.println("You are wrong");
+        }
+
+    }
+}
