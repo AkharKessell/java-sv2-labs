739a2caa63f38b94ff26797ab8c94e9c235818b0
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 19:26:51 +0100

    Gyakorlati feladat - Számjegyek kiválogatása
    A Digits osztálynak legyen egy List<Integer> típusú attribútuma, amelyet példányosíts! Legyen hozzá getter metódus, valamint készíts egy addDigitsToList(String text) metódust, amely a paraméterül kapott, bármilyen karaktereket tartalmazó szövegből kiválogatja a számjegyeket és a listához adja! Az osztály main() metódusában teszteld a metódus működését!

---
+++ training-solutions/src/main/java/conversions/Digits.java
@@ -0,0 +1,20 @@
+package src.main.java.conversions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Digits {
+    private List<Integer> digits = new ArrayList<>();
+
+    public List<Integer> getDigits() {
+        return digits;
+    }
+
+    public void addDigitsToList(String text) {
+        for (char c : text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits.add(Integer.parseInt(String.valueOf(c)));
+            }
+        }
+    }
+}
---
+++ training-solutions/src/main/java/conversions/DigitsMain.java
@@ -0,0 +1,9 @@
+package src.main.java.conversions;
+
+public class DigitsMain {
+    public static void main(String[] args) {
+        Digits digits = new Digits();
+        digits.addDigitsToList("D1i2g3i4t5s6");
+        System.out.println(digits.getDigits());
+    }
+}
