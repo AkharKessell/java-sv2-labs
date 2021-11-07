3425e373f1306a8dc4a2b36bb4dc5bd82ee26e76
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 19:40:28 +0100

    A LettersAndDigits osztályban legyen egy printLetterOrDigit(String text) metódus, amely paraméterként vár egy szöveget, benne összevissza mindenféle karakterrel, és az egyes karakterekre vonatkozóan kiírja a konzolra, hogy az az adott karakter betű, számjegy vagy egyéb karakter. Tehát egy ilyen szövegre: bfa68fF;X ilyesmi kiírást kell megvalósítani:
    b : betű
    f : betű
    a : betű
    6 : számjegy
    8 : számjegy
    f : betű
    F : betű
    ; : egyéb
    X : betű
    
    Az osztály main() metódusában teszteld!

---
+++ training-solutions/src/main/java/chars/LettersAndDigits.java
@@ -0,0 +1,17 @@
+package src.main.java.chars;
+
+public class LettersAndDigits {
+    public String printLetterOrDigit(String text) {
+        String mix = "";
+        for (char a : text.toCharArray())
+            if (Character.isAlphabetic(a)) {
+                mix += a + " : word" + "\n";
+            } else if (Character.isDigit(a)) {
+                mix += a + " : number" + "\n";
+            } else {
+                mix += a + " : other" + "\n";
+            }
+
+        return mix;
+    }
+}
---
+++ training-solutions/src/main/java/chars/LettersAndDigitsMain.java
@@ -0,0 +1,9 @@
+package src.main.java.chars;
+
+public class LettersAndDigitsMain {
+    public static void main(String[] args) {
+        LettersAndDigits lAndD = new LettersAndDigits();
+        System.out.println(lAndD.printLetterOrDigit("l3tt3rs&D1g1t8"));
+    }
+
+}
