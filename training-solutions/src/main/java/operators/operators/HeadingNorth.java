9b7dfc3663145ca152c3aa13e4361416cb3771c1
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-07 16:58:01 +0100

    Gyakorlati feladat - Arccal északnak
    A HeadingNorth osztály getNumberOfNewDirections() metódusát úgy írd meg, hogy a következő matematikai feladat megoldását adja vissza:
    
    Egy ember elindul észak felé. Pár méter után eltér a haladási irányától jobbra 10 fokos szöggel. Újabb pár méter után ettől a haladási irányától is eltér jobbra 20 fokos szöggel. Ezután mindig megy egy darabig, majd mindig eltér a haladási irányától jobbra 10 fokos szöggel többet, mint azt megelőzően. Hányszor tett ilyen irányváltoztatást addig, amíg újra megint észak felé haladt?
    
    Az osztály main() metódusában ellenőrizd a megoldásod!

---
+++ training-solutions/src/main/java/operators/operators/HeadingNorth.java
@@ -0,0 +1,17 @@
+package src.main.java.operators.operators;
+
+public class HeadingNorth {
+
+    public int getNumberOfNewDirections() {
+        int degree = 0;
+        int turn = 0;
+
+        for (int i = 0; i < 360; i += degree) {
+            degree += 10;
+            turn ++;
+        }
+        return turn;
+    }
+}
+
+
