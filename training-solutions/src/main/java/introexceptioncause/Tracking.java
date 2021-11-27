9d190ce2af69425a473021e974a1129afce61f11
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-27 21:04:10 +0100

    Gyakorlati feladat - Emelkedések és süllyedések
    Készíts egy tracking.csv fájlt, amelyben egy túrázás adatai vannak eltárolva. Ehhez másold bele a következő adatokat:
    
    47.497913;19.040236;400
    47.497912;19.040232;420
    47.497916;19.040236;410
    47.497915;19.040237;415
    47.497921;19.040232;420
    47.497922;19.040236;423
    47.497925;19.040236;418
    47.497931;19.040237;422
    47.497933;19.040238;425
    
    Itt GPS-koordinátákat látsz, valamint a hozzájuk tartozó magassági adatokat.
    
    Olvasd be a fájl tartalmát, majd írd ki a konzolra, hogy egyik pontról a másikra gyalogolva hány métert emelkedett vagy süllyedt a túrázó!
    
    A következő kiírást kell tehát megvalósítanod:
    
    20
    -10
    5
    5
    3
    -5
    4
    3
    
    Minden egyes műveletet szervezz ki külön metódusokba a TrackPoints osztályban, de ezeket a metódusokat a Tracking osztály main() metódusából hívd meg!

---
+++ training-solutions/src/main/java/introexceptioncause/Tracking.java
@@ -0,0 +1,13 @@
+package introexceptioncause;
+
+public class Tracking {
+    public static void main(String[] args) {
+        TrackPoints tracking = new TrackPoints();
+
+        try {
+            tracking.calculateElevation(tracking.readFile("src/main/resources/tracking.csv"));
+        } catch (IllegalStateException ise) {
+            System.out.println(ise.getMessage());
+        }
+    }
+}
