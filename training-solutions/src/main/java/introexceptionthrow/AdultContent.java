7f0b4b8ebcf414ecc7f0117fb70d0a8eb4ebbca1
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-27 12:07:10 +0100

    Gyakorlati feladat - Felnőtt tartalom
    Az Adult osztályban legyen két attribútum, String name és int age! Legyenek hozzájuk getterek, valamint hozz létre egy olyan konstruktort, amely kivételt dob, ha az illető életkora 18 év alatti! Teszteld az AdultContent osztályban!

---
+++ training-solutions/src/main/java/introexceptionthrow/AdultContent.java
@@ -0,0 +1,8 @@
+package introexceptionthrow;
+
+public class AdultContent {
+    public static void main(String[] args) {
+        Adult adult = new Adult("John", 20);
+        Adult adult2 = new Adult("George", 11);
+    }
+}
