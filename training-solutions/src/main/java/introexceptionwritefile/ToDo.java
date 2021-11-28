ae6c30e99301460d661e060da9e4b3eb15e4e30b
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-28 10:16:39 +0100

    Gyakorlati feladat - Teendők listája
    A ToDo osztályban készíts egy alkalmazást, amivel teendőket lehet bejegyezni egy listába! Írd ki a felhasználónak, hogy adja meg a következő todo-t, és amit megad, azt mentsd el egy listába! Majd ezután addig adhasson meg újabb todo-kat, amíg az “x” billentyű megnyomásával nem jelzi, hogy befejezte, ki lehet írni fájlba a teendőket! Ezután pedig írd ki a megadott teendőket egy todos.txt nevű fájlba!

---
+++ training-solutions/src/main/java/introexceptionwritefile/ToDo.java
@@ -0,0 +1,43 @@
+package introexceptionwritefile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ToDo {
+    private final List<String> stuffToDo = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ToDo toDo = new ToDo();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Add meg a következő teendőt. A rögzítést az X karakterrel tudod befejezni.");
+        String line;
+
+        while (!(line = scanner.nextLine()).equalsIgnoreCase("x")) {
+            toDo.addToList(line);
+        }
+        toDo.writeList(Paths.get("src/main/resources/todos.txt"));
+        System.out.println("Lista rögzítve.");
+    }
+
+    public List<String> getStuffToDo() {
+        return stuffToDo;
+    }
+
+    public void addToList(String toDo) {
+        stuffToDo.add(toDo);
+    }
+
+    public void writeList(Path path) {
+        try {
+            Files.write(path, stuffToDo);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot write file.", ioe);
+        }
+    }
+}
