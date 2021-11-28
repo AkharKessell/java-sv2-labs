4b5e41e12eed66ccb43dea16f1d4faea2b2efe42
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-28 11:39:26 +0100

    Gyakorlati feladat - Nagy összegű bankszámlák
    Hozz létre egy bankaccount.BankAccount osztályt a szükséges attribútumokkal:
    
        nameOfOwner, a számla tulajdonosának neve
        accountNumber, a számlaszám
        balance, egyenleg
    
    Feladat egy metódus megírása a BankAccountConditionCounter osztályban, ami megszámlálja, hány olyan számla van, amelynek az aktuális egyenlege meghaladja a paraméterként kapott alsó határt. A metódus nevét megtudhatod a tesztesetből.

---
+++ training-solutions/test/java/algorithmscount/bankaccount/BankAccountTest.java
@@ -0,0 +1,15 @@
+package algorithmscount.bankaccount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountTest {
+    @Test
+    void testCreate() {
+        BankAccount account = new BankAccount("Jack", "548454545-448454548", 1500);
+        assertEquals("Jack", account.getNameOfOwner());
+        assertEquals("548454545-448454548", account.getAccountNumber());
+        assertEquals(1500, account.getBalance());
+    }
+}
