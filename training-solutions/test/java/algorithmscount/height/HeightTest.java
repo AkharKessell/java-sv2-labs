a5ca1361a65fcaa28251e51ff74d2ea7920fbd4f
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-28 11:26:43 +0100

    Gyakorlati feladat - Magassági korlát
    Egy játszótéri eszközön csak egy bizonyos magasságnál magasabb gyerekek játszhatnak. A height.Height osztályban legyen egy countChildrenWithHeightGreaterThan() metódus, amelynek két paramétere egy egész számokat tartalmazó lista, amelyben egy csapatnyi gyerek centiméterben mért magassága található, valamint egy meghatározott magassági korlát (szintén centiméterben)! A metódus feladata, hogy adja vissza, az adott gyerekcsoportból hányan játszhatnak a játszótéri játékon.
    
    Írd meg a tesztet is a metódushoz a height.HeightTest osztályban!

---
+++ training-solutions/test/java/algorithmscount/height/HeightTest.java
@@ -0,0 +1,17 @@
+package algorithmscount.height;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeightTest {
+    @Test
+    void testCounter() {
+        Height height = new Height();
+
+        int count = height.countChildrenWithHeightGreaterThan(Arrays.asList(155, 160, 140, 139, 145), 145);
+        assertEquals(3, count);
+    }
+}
