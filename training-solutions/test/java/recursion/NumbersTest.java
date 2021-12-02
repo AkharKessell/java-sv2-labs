1f2ceb87d591b6344faa71bf75efe6deb088d00a
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-02 16:02:14 +0100

    Gyakorlati feladat - Tömb elemeinek összege
    A Numbers osztályban legyen egy getSum(int[] numbers) metódus, mely összeadja a paraméterül kapott, egész számokat tartalmazó tömb elemeit! Írd meg a feladatot rekurzióval! (A megoldás menete: Egy 10 elemű tömb összegét számoljuk úgy ki, hogy az első elemet hozzáadjuk a 9 elemű résztömb elemeinek összegéhez. A metódus önmagát hívja újra, ekkor már a 9 elemű tömbbel, ekkor már az első elemet a 8 elemű résztömb elemeinek összegéhez kell hozzáadni és így tovább. Amint a tömb 1 eleművé “fogy”, az elemeinek az összege egyértelműen adódik.)

---
+++ training-solutions/test/java/recursion/NumbersTest.java
@@ -0,0 +1,16 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumbersTest {
+
+    Numbers numbers = new Numbers();
+
+    @Test
+    void testNumbers() {
+        int result = numbers.getSum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        assertEquals(55, result);
+    }
+}
