0f07d763314224932f19fea2e95ac9c882c7999b
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-03 15:28:57 +0100

    Gyakorlati feladat - Háromszögmátrix
    A Triangular osztályban hozz létre egy int[][] triangularMatrix(int size) metódust, mely a paraméterként megadott méretű háromszögmátrixot hozza létre, és minden sora a sor számának értékeit tartalmazza! Elég, ha csak egyjegyű számokkal írható ki szépen a konzolra. Ilyen kiírást várunk:
    
    0
    1 1
    2 2 2
    3 3 3 3
    
    A main() metódusban teszteld le a metódus működését!

---
+++ training-solutions/src/main/java/arrayofarrays/TriangularMain.java
@@ -0,0 +1,12 @@
+package arrayofarrays;
+
+public class TriangularMain {
+
+    public static void main(String[] args) {
+
+        Triangular triangular = new Triangular();
+        ArrayOfArrays arrPrinter = new ArrayOfArrays();
+
+        arrPrinter.printArrayOfArrays(triangular.triangularMatrix(8));
+    }
+}
