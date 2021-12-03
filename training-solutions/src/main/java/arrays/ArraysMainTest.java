c557cc1cbb378bb157f17b6059d4fc9857fccaf8
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-03 15:31:07 +0100

    Gyakorlati feladat - Hónapok napjainak száma
    A String numberOfDaysAsString() metódusban definiálj egy numberOfDays nevű változót, mely a hónapok napjainak számát tartalmazza, és add vissza String-ként egy utasítással az értékeit.
    Gyakorlati feladat - Szorzótábla
    
    A multiplicationTableAsString(int size) metódus definiáljon egy size méretű szorzótáblát, és adja vissza az értékeket String-ként egy sorban.
    Gyakorlati feladat - Hőmérsékleti értékek
    
    A sameTempValues(double[] day, double[] anotherDay) hőmérsékleti értékeket vár, órai mérésekkel, két napra. Vizsgáld meg, hogy a paraméterként megadott két nap azonos méréseket tartalmazott-e!
    Gyakorlati feladat - Hőmérsékleti értékek és az óraátállítás
    
    A sameTempValuesDaylight(double[] day, double[] anotherDay) metódus ugyanúgy hasonlítsa össze az értékeket, de vegye figyelembe a 23 és 25 órás napokat is. Összehasonlítás során mindig a kisebb óraszámot vegye figyelembe alapként, és úgy hasonlítson össze (mindkettőn hívd meg a copyOfRange() metódust)!
    
    Implementálhatsz egy min(int, int) segédmetódust, mely a két óraszám közül a kisebbet adja vissza.
    Gyakorlati feladat - Lottószámok
    
    Döntsd el a boolean wonLottery(int[], int[]) metódusban, hogy a megtett számok, és a kihúzott számok megegyeznek-e! Nem biztos, hogy növekvő sorrendben vannak a számok. Azaz ellenőrizd, hogy ugyanazokat az értékeket tartalmazza-e a két paraméter, sorrendtől függetlenül!
    
    Válaszd azt a megoldást, hogy mindkét tömböt rendezed, és úgy hasonlítod őket össze!
    
    Miután a main() metódusban meghívtad a wonLottery() metódust, vizsgáld meg, hogy a paraméterként átadott tömb rendezett lett-e, azaz a rendezésnek lett-e visszahatása a paraméterként átadott tömbre! (Ehhez az kell, hogy a paraméterek változók legyenek, melyek értékét a hívás után vizsgálni lehet.)
    
    Ha igen, próbáld meg valahogy kikerülni a problémát, azaz a metódusnak ne legyen mellékhatása.

---
+++ training-solutions/src/main/java/arrays/ArraysMainTest.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArraysMainTest {
+    public static void main(String[] args) {
+        ArraysMain arr = new ArraysMain();
+
+        System.out.println("Hónapok napjai:\n" + arr.numberOfDaysAsString());
+        System.out.println();
+
+        System.out.println("6-os szorzótábla:\n" + arr.multiplicationTableAsString(6));
+        System.out.println();
+
+        double[] day = {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+        double[] anotherDay = {10.0, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+
+        System.out.println("Napok összehasonlítása:\n" + arr.sameTempValues(day, anotherDay));
+        System.out.println();
+
+        double[] shortDay = {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9};
+        double[] longDay = {5.1, 5.5, 6.0, 7.3, 8.0, 10.2, 9.5,
+                10.6, 11.5, 15.6, 18.9, 20.0, 18.5, 16.3, 15.5,
+                15.0, 14.1, 13.0, 13.0, 12.0, 11.1, 11.0, 9.9, 9.9, 8.0};
+
+        System.out.println("Eltérő hosszúságú napok összehasonlítása:\n" + arr.sameTempValuesDayLight(shortDay, longDay));
+        System.out.println();
+
+        int[] playedNumbers = {32, 60, 12, 20, 75};
+        int[] winningNumbers = {90, 64, 71, 60, 30};
+
+        System.out.println("Megjátszott számok sorbarendezés előtt: " + Arrays.toString(playedNumbers));
+        System.out.println("Nyert a lottón?\n" + arr.wonLottery(playedNumbers, winningNumbers));
+        System.out.println("Megjátszott számok sorbarendezés után: " + Arrays.toString(playedNumbers));
+
+    }
+}
