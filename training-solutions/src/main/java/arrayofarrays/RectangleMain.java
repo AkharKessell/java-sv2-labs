bb648ae03b36a308a4ad2097e2d74774cf999545
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-03 15:29:15 +0100

    yakorlati feladat - Négyszögmátrix
    A Rectangle osztályban hozz létre egy int[][] rectangularMatrix(int size) metódust, mely a paraméterként megadott méretű négyszögmátrixot hozza létre, és minden sora a sor számának értékeit tartalmazza! Elég, ha csak egyjegyű számokkal írható ki szépen a konzolra. Ilyen kiírást várunk:
    
    0 0 0 0
    1 1 1 1
    2 2 2 2
    3 3 3 3

---
+++ training-solutions/src/main/java/arrayofarrays/RectangleMain.java
@@ -0,0 +1,12 @@
+package arrayofarrays;
+
+public class RectangleMain {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        ArrayOfArrays arrPrinter = new ArrayOfArrays();
+
+        arrPrinter.printArrayOfArrays(rectangle.rectangularMatrix(4));
+    }
+}
