e39cbe63f6cde40dc343d62f4dcf1a0235c319ef
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-02 18:11:46 +0100

    Gyakorlati feladat - Hosszú szó
    Hozz létre a test ág resources könyvtárában egy longword.txt nevű fájlt, a következő tartalommal (használhatsz más szót is, másmilyen mátrixban megadva):
    
    LONG
    ONGW
    NGWO
    GWOR
    WORD
    
    Hozz létre egy FileOperations interfészt, amely tartalmaz egy default metódust (default List<String> readFromFile(Path path)), amelyben beolvassa a file tartalmát! Legyen az interfészben egy absztrakt metódus is: String getLongWord()! A LongWord osztály implementálja az interfészt, és a getLongWord() metódusa adja vissza a mátrixban rejlő szót! (A szó többféle módon és irányban is kiolvasható a mátrixból, pl. az első sort és a többi sorok utolsó betűjét egymás után olvasva, de más megoldás is jó lehet.)
    
    A LongWordTest osztályban írd is meg a tesztesetet a metódus működésének ellenőrzésére!

---
+++ training-solutions/src/test/java/interfacedefaultmethods/longword/LongWordTest.java
@@ -0,0 +1,25 @@
+package interfacedefaultmethods.longword;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LongWordTest {
+
+    @Test
+    void testGetLongWord() {
+        LongWord longWord = new LongWord(Paths.get("longword.txt"));
+        assertEquals("LONGWORD", longWord.getLongWord());
+    }
+
+    @Test
+    void testWrongFile() {
+        LongWord longWord = new LongWord(Paths.get("src/test/resources/longwdfeeord.txt"));
+        IllegalArgumentException ioe = assertThrows(IllegalArgumentException.class,
+                () -> longWord.getLongWord());
+        assertEquals("Can't read file.", ioe.getMessage());
+    }
+}
