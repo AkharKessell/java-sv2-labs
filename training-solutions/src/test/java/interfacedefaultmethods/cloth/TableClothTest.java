93b1e21a7ef39548aa74c6f47cdd15fc6f8b090f
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-02 18:00:32 +0100

    Gyakorlati feladat - Négyzet alakú terítő
    Készíts egy cloth.Square nevű interfészt, melyben négy default metódus is található:
    
        int getNumberOfSides(): visszaadja egy négyzet oldalainak számát.
        double getLengthOfDiagonal(): az oldal hosszának ismeretében visszaadja a négyzet átlójának hosszát.
        double getPerimeter(): az oldal hosszának ismeretében visszaadja a négyzet kerületét.
        double getArea(): az oldal hosszának ismeretében visszaadja a négyzet területét.
    
    Legyen az interfészben egy absztrakt double getSide() metódus is! A default metódusok ezt a metódust hívják meg a visszaadott értékeik kiszámolásához!
    
    Legyen egy TableCloth osztály, amely implementálja ezt az interfészt, és van egy double side attribútuma, melynek értékét konstruktorban állítja be! A getSide() metódus ezt adja vissza!
    
    Írj tesztet a TableClothTest osztályban a metódusok működésének ellenőrzésére!

---
+++ training-solutions/src/test/java/interfacedefaultmethods/cloth/TableClothTest.java
@@ -0,0 +1,35 @@
+package interfacedefaultmethods.cloth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TableClothTest {
+    TableCloth tableCloth = new TableCloth(10);
+
+    @Test
+    void testGetArea() {
+        assertEquals(100, tableCloth.getArea());
+    }
+
+    @Test
+    void testGetSide() {
+        assertEquals(10, tableCloth.getSide());
+    }
+
+    @Test
+    void testGetNumberOfSides() {
+        assertEquals(4, tableCloth.getNumberOfSides());
+    }
+
+    @Test
+    void testGetLengthOfDiagonal() {
+        assertEquals(14.142, tableCloth.getLengthOfDiagonal(), 0.001);
+    }
+
+    @Test
+    void testGetPerimeter() {
+        assertEquals(40, tableCloth.getPerimeter());
+    }
+
+}
