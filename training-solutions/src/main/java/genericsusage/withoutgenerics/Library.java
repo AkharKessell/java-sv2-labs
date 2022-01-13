57ac6b61dfedaaf3f0635c31be5a77daac4f7ed9
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-13 11:38:21 +0100

    Gyakorlati feladat - Generikusok használatával
    Készíts egy Book osztályt, egyetlen attribútummal: String title Implementálj egy könytárat (withgenerics.Library), ahol könyveket tárolhatunk! Valósítsd meg a getFirstBook(List<Book> books) metódust generikusok használatával! Ha a könyvtárat reprezentáló kollekció null, a metódus dobjon NullPointerException kivételt! Ha a könyvtárat reprezentáló kollekció üres, a metódus dobjon IllegalArgumentException kivételt!
    Gyakorlati feladat - Generikusok használata nélkül
    
    Készíts egy Book osztályt, egyetlen attribútummal: String title Implementálj egy könytárat (withoutgenerics.Library), ahol könyveket tárolhatunk! Valósítsd meg a getFirstBook(List books) metódust generikusok használata nélkül! Ha a könyvtárat reprezentáló kollekció null, a metódus dobjon NullPointerException kivételt! Ha a könyvtárat reprezentáló kollekció üres, a metódus dobjon IllegalArgumentException kivételt!

---
+++ training-solutions/src/main/java/genericsusage/withoutgenerics/Library.java
@@ -0,0 +1,16 @@
+package genericsusage.withoutgenerics;
+import genericsusage.Book;
+import java.util.List;
+
+public class Library {
+
+    public Book getFirstBook(List books) {
+        if (books == null) {
+            throw new NullPointerException("Can't be null!");
+        } // Ha a könyvtárat reprezentáló kollekció null, a metódus dobjon NullPointerException kivételt!
+        if (books.size() == 0) {
+            throw new IllegalArgumentException("Can't be empty!");
+        } // Ha a könyvtárat reprezentáló kollekció üres, a metódus dobjon IllegalArgumentException kivételt!
+        return (Book) books;
+    }
+}
